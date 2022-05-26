228d66162417dcb41377833d463cf867c06f983c
Raubel <raubel1993@gmail.com>
2022-05-26 19:59:48 -0400

    templates

--- src/Module.php
+++ src/Module.php
@@ -7,30 +7,21 @@ class Module extends \yii\gii\Module
     protected function coreGenerators()
     {
         return [
-            'model' => ['class' => 'yii\gii\generators\model\Generator'],
-            'crud' => ['class' => 'yii\gii\generators\crud\Generator'],
-            'controller' => ['class' => 'yii\gii\generators\controller\Generator'],
-            'form' => ['class' => 'yii\gii\generators\form\Generator'],
-            'module' => ['class' => 'yii\gii\generators\module\Generator'],
-            'extension' => ['class' => 'yii\gii\generators\extension\Generator'],
             'model' => [
                 'class' => 'yii\gii\generators\model\Generator',
                 'templates' => [ //setting for out templates
-                    'default' => '@generators/model/api-rest', // template name => path to template
+                    'default' => '@rguerral/api/model/api-rest', // template name => path to template
                 ]
             ],
             'crud' => [
                 'class' => 'yii\gii\generators\crud\Generator',
-                /*'templates' => [ //setting for out templates
-                    'myCrud' => '@app/template/crud/default', // template name => path to template
-                ]*/
             ],
             'controller' => [
-                'class' => 'generators\controller\Generator',
+                'class' => 'rguerral\api\controller\Generator',
                 'baseClass' => 'yii\rest\Controller',
                 'actions' => '',
                 'templates' => [ //setting for out templates
-                    'default' => '@generators/controller/api-rest', // template name => path to template
+                    'default' => '@rguerral/api/controller/api-rest', // template name => path to template
                 ]
             ],
             'form' => [
